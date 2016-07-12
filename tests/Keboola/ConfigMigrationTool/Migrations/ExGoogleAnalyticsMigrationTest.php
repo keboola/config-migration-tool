1c483e6cf66ac8b3a5286ba5f10b936d69b1a7ba
Miroslav Cillik <miro@keboola.com>
2016-07-12 15:27:38 +0200

    fix: ga tests

--- tests/Keboola/ConfigMigrationTool/Migrations/ExGoogleAnalyticsMigrationTest.php
+++ tests/Keboola/ConfigMigrationTool/Migrations/ExGoogleAnalyticsMigrationTest.php
@@ -9,6 +9,7 @@
 namespace Keboola\ConfigMigrationTool\Test;
 
 use Keboola\ConfigMigrationTool\Migration\ExGoogleAnalyticsMigration;
+use Keboola\StorageApi\Options\Components\Configuration;
 use Monolog\Logger;
 
 class ExGoogleAnalyticsMigrationTest extends ExGoogleAnalyticsTest
@@ -18,6 +19,36 @@ class ExGoogleAnalyticsMigrationTest extends ExGoogleAnalyticsTest
         $testConfigIds = $this->createOldConfigs();
         $migration = new ExGoogleAnalyticsMigration($this->getLogger());
         $createdConfigurations = $migration->execute();
+
+        /** @var Configuration $configuration */
+        foreach ($createdConfigurations as $configuration) {
+            $this->assertEquals('keboola.ex-google-analytics-v4', $configuration->getComponentId());
+            $config = $configuration->getConfiguration();
+            $this->assertArrayHasKey('authorization', $config);
+            $this->assertArrayHasKey('parameters', $config);
+            $parameters = $config['parameters'];
+            $this->assertArrayHasKey('outputBucket', $parameters);
+            $this->assertArrayHasKey('queries', $parameters);
+            $queries = $parameters['queries'];
+            foreach ($queries as $query) {
+                $this->assertArrayHasKey('id', $query);
+                $this->assertArrayHasKey('name', $query);
+                $this->assertArrayHasKey('query', $query);
+                $this->assertArrayHasKey('outputTable', $query);
+                $this->assertArrayHasKey('enabled', $query);
+                $this->assertArrayHasKey('metrics', $query['query']);
+                $metric = $query['query']['metrics'][0];
+                $this->assertArrayHasKey('expression', $metric);
+                $this->assertArrayHasKey('query', $query);
+                $this->assertArrayHasKey('dimensions', $query['query']);
+                $dimension = $query['query']['dimensions'][0];
+                $this->assertArrayHasKey('name', $dimension);
+                $this->assertArrayHasKey('viewId', $query['query']);
+                $this->assertArrayHasKey('dateRanges', $query['query']);
+            }
+        }
+
+        //@todo: clear created configurations
     }
 
     private function getLogger()
