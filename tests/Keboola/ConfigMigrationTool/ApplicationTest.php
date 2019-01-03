fc509f5eb4e7aa67a67498767ea12f5daacbf6d4
Jakub Matejka <jakub@keboola.com>
2019-01-03 10:27:32 +0100

    fix: app tests

--- tests/Keboola/ConfigMigrationTool/ApplicationTest.php
+++ tests/Keboola/ConfigMigrationTool/ApplicationTest.php
@@ -6,7 +6,6 @@ namespace Keboola\ConfigMigrationTool\Test;
 
 use Keboola\ConfigMigrationTool\Application;
 use Keboola\ConfigMigrationTool\Exception\UserException;
-use Keboola\ConfigMigrationTool\Migration\ExGoogleDriveMigration;
 use Keboola\ConfigMigrationTool\Migration\GenericCopyMigration;
 use Monolog\Handler\NullHandler;
 use Monolog\Logger;
@@ -25,33 +24,53 @@ class ApplicationTest extends TestCase
         );
     }
 
+    protected function getConfigWithParameters(array $parameters) : array
+    {
+        return $this->getConfig(['parameters' => $parameters]);
+    }
+
+    protected function getConfig(array $config) : array
+    {
+        return array_merge([
+            'image_parameters' => [
+                'gooddata_provisioning_url' => 'https://gooddata-provisioning.keboola.com',
+                'gooddata_url' => 'https://secure.gooddata.com',
+                '#production_token' => 'production',
+                '#demo_token' => 'demo',
+                'gooddata_writer_url' => 'https://syrup.keboola.com/gooddata-writer',
+                '#manage_token' => 'token',
+                'project_access_domain' => 'kbc.keboola.com',
+            ],
+        ], $config);
+    }
+
     public function testApplicationGetMigration() : void
     {
         try {
-            $this->application->getMigration(['parameters' => ['component' => 'ex-google-analyticsx']]);
+            $this->application->getMigration($this->getConfigWithParameters(['component' => 'ex-google-analyticsx']));
             $this->fail();
         } catch (UserException $e) {
         }
 
-        $result = $this->application->getMigration(['parameters' => [
+        $result = $this->application->getMigration($this->getConfigWithParameters([
             'origin' => 'ex-adwords-v2', 'destination' => 'keboola.ex-adwords-v201705',
-        ]]);
+        ]));
         $this->assertInstanceOf(GenericCopyMigration::class, $result);
 
-        $this->application->getMigration(['parameters' => [
+        $this->application->getMigration($this->getConfigWithParameters([
             'origin' => 'ex-adwords-v2x', 'destination' => 'keboola.ex-adwords-v201705',
-        ]]);
+        ]));
         $this->assertInstanceOf(GenericCopyMigration::class, $result);
 
-        $this->application->getMigration(['parameters' => [
+        $this->application->getMigration($this->getConfigWithParameters([
             'origin' => 'ex-adwords-v2', 'destination' => 'keboola.ex-adwords-v201705x',
-        ]]);
+        ]));
         $this->assertInstanceOf(GenericCopyMigration::class, $result);
     }
 
     public function testApplicationGetSupportedMigrations() : void
     {
-        $res = $this->application->action(['action' => 'supported-migrations']);
+        $res = $this->application->action($this->getConfig(['action' => 'supported-migrations']));
         $this->assertArrayHasKey('ex-adwords-v2', $res);
         $this->assertCount(1, $res['ex-adwords-v2']);
         $this->assertContains('keboola.ex-adwords-v201705', $res['ex-adwords-v2']);
